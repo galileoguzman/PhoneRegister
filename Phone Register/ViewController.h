658982f2296bc352aa8912d3386ef53b0f953541
Galileo Guzmán <galileoguzman@gmail.com>
2015-11-27 20:50:37 -0600

    Initial Commit

---
+++ Phone Register/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Phone Register
+//
+//  Created by Galileo Guzman on 27/11/15.
+//  Copyright © 2015 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Phone Register/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Phone Register
+//
+//  Created by Galileo Guzman on 27/11/15.
+//  Copyright © 2015 Galileo Guzman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
